b0966457e91fd788bb3afa544ec20ccd2d35ebcf
kauanny-cmd <kauannyvieira8@gmail.com>
2022-09-12 16:21:07 -0300

    :sparkles:	feat: Formatação

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,75 @@
 const date = new Date();
 
 const render = () => {
-    date.setDate(1);
+  date.setDate(1);
 
-    const monthDays = document.querySelector('.days');
+  const monthDays = document.querySelector('.days');
 
-    const lastDay = new Date(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        0
-    ).getDate();
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
 
-    const prevLastDays = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        0
-    ).getDate();
+  const prevLastDays = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
 
-    const firstDayIndex = date.getDay();
+  const firstDayIndex = date.getDay();
 
-    const lastDayIndex = new Date(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        0
-    ).getDay();
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
 
-    const nextDay = 7 - lastDayIndex - 1;
+  const nextDay = 7 - lastDayIndex - 1;
 
-    const months = [
-        "Janeiro", "Fevereiro", "Março", "Abril","Maio","Junho",
-        "Julho","Agosto", "Setembro", "Outubro", "Novembro", "Dezembro",
-    ];
+  const months = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro",
+  ];
 
-    document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+  document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 
-    let days = "";
+  let days = "";
 
-    for (let x = firstDayIndex; x > 0; x--) {
-      days += `<div class="prev-date">${prevLastDays - x + 1}</div>`;
-    }
-  
-    for (let i = 1; i <= lastDay; i++) {
-      if (
-        i === new Date().getDate() &&
-        date.getMonth() === new Date().getMonth()
-      ) {
-        days += `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDays - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
        <div class="today">${i}</div>
       </button>
       
       `;
-      } else {
-        days += `<div>${i}</div>`;
-      }
+    } else {
+      days += `<div>${i}</div>`;
     }
-  
-    for (let j = 1; j <= nextDay; j++) {
-      days += `<div class="next-date">${j}</div>`;
-      monthDays.innerHTML = days;
-    }
+  }
+
+  for (let j = 1; j <= nextDay; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
 
 };
 
 document.querySelector(".prev").addEventListener("click", () => {
-    date.setMonth(date.getMonth() - 1);
-    render();
-  });
-  
-  document.querySelector(".next").addEventListener("click", () => {
-    date.setMonth(date.getMonth() + 1);
-    render();
-  });
+  date.setMonth(date.getMonth() - 1);
+  render();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  render();
+});
 
 render();
